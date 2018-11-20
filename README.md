eb417d988944d04666dafa19d983f3bac24f66ae
Jonathan Saindon <jonathan.saindon@gmail.com>
2018-11-20 01:52:48 -0500

    Initial playbook with install script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # raspi-music-server
+Pi Musicbox server environment and configurations
+
+## Installation
+Run ```./install.sh``` to launch the playbook installation
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###################
+# Install ansible #
+###################
+if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    echo "Adding Ansible PPA"
+    sudo apt-add-repository ppa:ansible/ansible -y
+fi
+
+if ! hash ansible >/dev/null 2>&1; then
+    echo "Ansible installation not found !"
+    echo "Installing Ansible..."
+    sudo apt-get install ansible -y
+fi
+
+################
+# Run playbook #
+################
+ansible-playbook playbook.yml --ask-become-pass
