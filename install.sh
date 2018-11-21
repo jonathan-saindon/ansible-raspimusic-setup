3161c593ca802eed8d5de419061da16726685faf
Jonathan Saindon <jonathan.saindon@gmail.com>
2018-11-21 00:57:55 -0500

    Fixed install script and Removed system role

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@
 ###################
 if ! hash ansible >/dev/null 2>&1; then
     echo "Ansible installation not found !"
-    echo "Installing Ansible..."
-    sudo apt-get install ansible -y
+    echo "Installing Ansible and all its dependencies..."
+    sudo apt-get install libffi-dev python-pip -y > /dev/null
+    echo "Initial install can take several minutes"
+    sudo pip install ansible > /dev/null
+    echo "Done"
+    ansible --version
 fi
 
 ################
