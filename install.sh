4598c35a9a421028f89d6384ddf5add549bc91a8
Jonathan Saindon <jonathan.saindon@gmail.com>
2018-11-27 21:14:20 -0500

    Stabilised install script

--- install.sh
+++ install.sh
@@ -6,10 +6,16 @@
 if ! hash ansible >/dev/null 2>&1; then
     echo "Ansible installation not found !"
     echo "Installing Ansible and all its dependencies..."
-    sudo apt-get install libffi-dev python-pip -y > /dev/null
+
+    sudo apt-get install libffi-dev -y >> /dev/null
+    sudo easy_install python-pip -y >> install.log
+
     echo "Initial install can take several minutes"
-    sudo pip install ansible > /dev/null
+
+    sudo pip install PyYAML ansible >> install.log
+
     echo "Done"
+
     ansible --version
 fi
 
